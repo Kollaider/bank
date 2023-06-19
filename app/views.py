d19c5e049829c76e2219e1d3d86131e97973c7ad
Kollaider <kolja87@tut.by>
2023-06-19 21:53:23 +0300

    simple template, urls and views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
+from django.db.models import F
 from django.shortcuts import render
+from app.models import LoanApplication
 
-# Create your views here.
+
+def index(request):
+    """View with getting context for HomePage"""
+    loan_applications = LoanApplication.objects.all()
+    return render(request, 'app/index.html', {'loan_application': loan_applications})
+
+
+def loan_application(request, pk):
+    """View for handling LaonApllication info"""
+    uniq_man_pk = LoanApplication.objects.filter(pk=pk).\
+        annotate(uniq=F('products__manufacturer__pk')).\
+        exclude(uniq__isnull=True).\
+        values('uniq')
+    return render(request, 'app/index.html', {'uniq_man_pk': uniq_man_pk})
+
--- bank/urls.py
+++ bank/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from app.views import index, loan_application
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='home'),
+    path('loan_application/<int:pk>/', loan_application,  name='loan_app'),
 ]
